b41905a7c5c16772deefce004d753644770afc86
Dacooleric <dacooleric@gmail.com>
2022-07-17 19:44:57 +0100

    added a few changes and a Contact component

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import SignIn from "./ContactMUI";
+
 function Contact() {
-  return <div style={{ marginTop: "100px" }}>Contact</div>;
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <SignIn />
+    </div>
+  );
 }
 
 export default Contact;
